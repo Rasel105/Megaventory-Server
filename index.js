6c1e080e20689b3411fc4c1bb6f8e2fca2537527
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-04-30 03:13:22 +0600

    server is ready for creating api

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
+
 // username: Rasel 
 // pass: lIP6xROjO2R2e3tk
 
 app.get('/', (req, res) => {
     res.send("Running Inventory server");
 })
-
 
 app.listen(port, () => {
     console.log("Listening to port", port);
