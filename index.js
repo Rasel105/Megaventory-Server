176f1846f86503281c9f48c567dc288d735ab3f1
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-04-30 12:37:55 +0600

    check

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -10,6 +11,29 @@ app.use(express.json());
 // username: Rasel 
 // pass: lIP6xROjO2R2e3tk
 
+const uri = "mongodb+srv://Rasel:lIP6xROjO2R2e3tk@cluster0.lcyo8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollenction = client.db("FridgeInventory").collection("products");
+        
+        // GET API 
+        // http://localhost:5000/products
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollenction.find(query);
+            const products = await cursor.toArray({});
+            res.send(products);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 app.get('/', (req, res) => {
     res.send("Running Inventory server");
 })
@@ -20,11 +44,4 @@ app.listen(port, () => {
 
 
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://Rasel:<password>@cluster0.lcyo8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+
